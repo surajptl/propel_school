fb5902f0b807f81526deb2d7689c6497d62a1a09
Suraj Patel <suraj.patel@mountblue.io>
2019-10-17 13:59:56 +0530

    add search_fields

--- app/admin.py
+++ app/admin.py
@@ -3,7 +3,7 @@ from .models import Applicant, BatchDetail
 
 class BatchDetailAdmin(admin.ModelAdmin):
     list_display = ('batch_type', 'date_from', 'to_date', 'strength', 'mentor_name')
-
+    search_fields = ('batch_type', 'date_from', 'to_date', 'mentor_name')
 
 
 # Register your models here.
