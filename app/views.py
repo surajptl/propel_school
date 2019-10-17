5a04d4ee2c1fad2cf31c589f864383bcae2364e5
Suraj Patel <suraj.patel@mountblue.io>
2019-10-17 19:06:59 +0530

    remove login again

--- app/views.py
+++ app/views.py
@@ -5,8 +5,6 @@ from django.http import HttpResponse
 from .forms import ApplicationForm
 
 # Create your views here.
-
-
 def index(request):
     return render(request, 'app/index.html')
 
@@ -15,9 +13,6 @@ def application(request):
     if request.method == 'POST':
         form = ApplicationForm(request.POST)
         if form.is_valid():
-        # name = form.cleaned_data['applicant_name']
-        # print(name)
-            print('here')
             form.save()
     form = ApplicationForm()
     return render(request, 'form.html', {'form':form})
