2a825d76788c1e90b895db0d887b80a4aaedb5fe
Shreekar Waghela <shreekar.waghela@mountblue.io>
2019-10-18 12:22:22 +0530

    postgres used

--- propel_school/settings.py
+++ propel_school/settings.py
@@ -81,18 +81,18 @@ WSGI_APPLICATION = 'propel_school.wsgi.application'
 # https://docs.djangoproject.com/en/2.2/ref/settings/#databases
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-    }
     # 'default': {
-    #     'ENGINE': config('ENGINE'),
-    #     'NAME': config('NAME'),
-    #     'USER': 'propelschool',
-    #     'PASSWORD': config('PASSWORD'),
-    #     'HOST': config('HOST'),
-    #     'PORT': config('PORT'),
+    #     'ENGINE': 'django.db.backends.sqlite3',
+    #     'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
     # }
+    'default': {
+        'ENGINE': config('ENGINE'),
+        'NAME': config('NAME'),
+        'USER': 'propelschool',
+        'PASSWORD': config('PASSWORD'),
+        'HOST': config('HOST'),
+        'PORT': config('PORT'),
+    }
 }
 
 
