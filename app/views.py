54f932ee0d8aab73d699ea602fa8635c82e2a305
Rajath Chandra S Shetty <rajath.shetty@mountblue.io>
2019-10-18 10:56:20 +0530

    applicant id genearted

--- app/views.py
+++ app/views.py
@@ -3,6 +3,7 @@ from django.contrib.auth.decorators import login_required
 from django.contrib.auth.forms import UserCreationForm 
 from django.http import HttpResponse 
 from .forms import ApplicationForm
+from users.models import CustomUser
 
 # Create your views here.
 
@@ -11,16 +12,14 @@ def index(request):
     return render(request, 'app/index.html')
 
 
+@login_required
 def application(request):
     if request.method == 'POST':
         form = ApplicationForm(request.POST)
-        
         if form.is_valid():
-        # name = form.cleaned_data['applicant_name']
-        # print(name)
-            print('here')
-            form.save()
+            instance              = form.save(commit=False)
+            instance.applicant_id = CustomUser.objects.get(id=request.user.id)
+            instance.save()
+            print(request.user.email)
     form = ApplicationForm()
-    return render(request, 'form.html', {'form':form})
-
-
+    return render(request, 'app/form.html', {'form':form})
