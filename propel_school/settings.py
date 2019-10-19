7f87003de082c326702e76b26462d64ebed085d8
Suraj Patel <suraj.patel@mountblue.io>
2019-10-19 20:54:56 +0530

    add installed app

--- propel_school/settings.py
+++ propel_school/settings.py
@@ -32,6 +32,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'app.config.SuitConfig',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
