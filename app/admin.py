6725e3dc89b4e795592f0665a4c62141c8c8c506
Suraj Patel <suraj.patel@mountblue.io>
2019-10-19 14:04:14 +0530

    add fcc points function

--- app/admin.py
+++ app/admin.py
@@ -6,6 +6,35 @@ class ApplicantAdmin(admin.ModelAdmin):
     list_display = ('applicant_name', 'applicant_id', 'phone_number', 'approval',)
     list_filter = ('applicant_name', 'approval',)
     search_fields = ('applicant_id__email','applicant_id__full_name')
+    actions = ('fetch_fcc_points', 'private_profile', 'not_enough_points', 'wrong_link', 'shortlist_condidate', \
+        'join_propel', 'propel_challenge', 'Extended_propel_challenge')
+
+    def private_profile(self, request, queryset):
+        queryset.update(approval='2')
+        print(dir(queryset.values()))
+
+    def not_enough_points(self, request, queryset):
+        queryset.update(approval='3')
+
+    def wrong_link(self, request, queryset):
+        queryset.update(approval='4')
+
+    def shortlist_condidate(self, request, queryset):
+        queryset.update(approval='5')
+
+    def join_propel(self, request, queryset):
+        queryset.update(approval='6')
+
+    def propel_challenge(self, request, queryset):
+        queryset.update(approval='7')
+
+    def Extended_propel_challenge(self, request, queryset):
+        queryset.update(approval='8')
+
+    def fetch_fcc_points(self, request, queryset):
+        pass
+
+
 
 
 class BatchDetailAdmin(admin.ModelAdmin):
